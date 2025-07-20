e4d5efce70e1ca59b8f7ec0a3b9d0b2397737b4f
opmartins <opmartins@outlook.com>
2025-07-20 16:35:11 -0300

    Inclusão do while para contar livros.

--- src/CalculadoraDeEstoque.java
+++ src/CalculadoraDeEstoque.java
@@ -1,10 +1,23 @@
 public class CalculadoraDeEstoque {
     public static void main(String[] args) {
-        double livroJava8 = 59.90;
-        double livroTDD = 59.90;
+       
+        double soma = 0;
+        int contador = 1;
 
-        double soma = livroJava8 + livroTDD;
+        while (contador <=35) {
+            double valorDoLivro = 59.90;
+            soma += valorDoLivro;
+            contador++;
+        }
 
         System.out.println("O valor total em estoque é: " + soma);
+
+        if (soma < 150) {
+            System.out.println("Seu estoque está baixo!");
+        } else if (soma >= 2000) {
+            System.out.println("Seu estoque está muito alto.");
+        } else {
+            System.out.println("Seu estoque está bom.");
+        }
     }
 }
