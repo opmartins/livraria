d51e4f60a7cc775a280e74380764f4821f002698
opmartins <opmartins@outlook.com>
2025-07-20 15:42:54 -0300

    first commit

---
+++ src/CalculadoraDeEstoque.java
@@ -0,0 +1,10 @@
+public class CalculadoraDeEstoque {
+    public static void main(String[] args) {
+        double livroJava8 = 59.90;
+        double livroTDD = 59.90;
+
+        double soma = livroJava8 + livroTDD;
+
+        System.out.println("O valor total em estoque é: " + soma);
+    }
+}
