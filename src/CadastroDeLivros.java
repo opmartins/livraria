6e89b91be10de1bf899f85cf5216e0b1f4068138
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-07-20 22:04:58 -0300

    Creating new classes. Removing old classes.

--- src/CadastroDeLivros.java
+++ src/CadastroDeLivros.java
@@ -1,26 +1,32 @@
 public class CadastroDeLivros {
     public static void main (String[] args) {
 
+        Autor autor = new Autor();
+        autor.nome = "Rodrigo Turini";
+        autor.email = "rodrigo.turini@caelum.com.br";
+        autor.cpf = "123.456.789.10";
+
         Livro livro = new Livro();
         livro.nome = "Java 8 Prático";
         livro.descricao = "Novos recursos da linguagem";
         livro.valor = 59.90;
         livro.isbn = "978-85-66250-46-6";
+        livro.autor = autor;
 
-        System.out.println("Nome: " + livro.nome);
-        System.out.println("Descrição: " + livro.descricao);
-        System.out.println("Valor: " + livro.valor);
-        System.out.println("ISBN: " + livro.isbn);
+        livro.mostrarDetalhes();
+
+        Autor outroAutor = new Autor();
+        outroAutor.nome = "Paulo Silveira";
+        outroAutor.email = "paulo.silveira@caelum.com.br";
+        outroAutor.cpf = "123.456.789.10";
 
         Livro outroLivro = new Livro();
         outroLivro.nome = "Lógica de Programação";
         outroLivro.descricao = "Crie seus primeiros programas";
         outroLivro.valor = 59.90;
         outroLivro.isbn = "978-85-66250-22-0";
+        outroLivro.autor = outroAutor;
 
-        System.out.println("Nome: " + outroLivro.nome);
-        System.out.println("Descrição: " + outroLivro.descricao);
-        System.out.println("Valor: " + outroLivro.valor);
-        System.out.println("ISBN: " + outroLivro.isbn);
+        outroLivro.mostrarDetalhes();
     }
 }
--- src/Livro.java
+++ src/Livro.java
@@ -3,13 +3,14 @@ public class Livro {
     String descricao;
     double valor;
     String isbn;
+    Autor autor;
 
     void mostrarDetalhes() {
-        String mensagem = "Mostrando detalhes do livro.";
-        System.out.println(mensagem);
-        System.out.println(nome);
-        System.out.println(descricao);
-        System.out.println(valor);
-        System.out.println(isbn);
+        System.out.println("Mostrando detalhes do livro: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("--");
     }
 }
