bc725960e6be493a463d0b0f409f814300767939
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-07-23 11:44:45 -0300

    Inclusão de método para verificar se o livro possui autor (teAutor).

--- src/Livro.java
+++ src/Livro.java
@@ -11,7 +11,19 @@ public class Livro {
         System.out.println("Descrição: " + descricao);
         System.out.println("Valor: " + valor);
         System.out.println("ISBN: " + isbn);
-        autor.mostrarDetalhes();
+        
+        if (this.temAutor()) {
+            autor.mostrarDetalhes();
+        }
+
         System.out.println("--");
+    }
+
+    public void aplicaDescontoDe(double porcentagem) {
+        this.valor -= this.valor * porcentagem;
+    }
+
+    boolean temAutor() {
+        return this.autor != null;
     }
 }
