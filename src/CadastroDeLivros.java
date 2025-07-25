e221a66d5e51e4f7375365b868c683e95de15350
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-07-25 13:51:00 -0300

    Removendo objeto outroAutor.

--- src/CadastroDeLivros.java
+++ src/CadastroDeLivros.java
@@ -17,19 +17,5 @@ public class CadastroDeLivros {
             System.out.println("Desconto não pode ser maior do que 30%");
         }
         livro.mostrarDetalhes();
-
-        Autor outroAutor = new Autor();
-        outroAutor.nome = "Paulo Silveira";
-        outroAutor.email = "paulo.silveira@caelum.com.br";
-        outroAutor.cpf = "123.456.789.10";
-
-        Livro outroLivro = new Livro();
-        outroLivro.nome = "Lógica de Programação";
-        outroLivro.descricao = "Crie seus primeiros programas";
-        outroLivro.valor = 59.90;
-        outroLivro.isbn = "978-85-66250-22-0";
-        outroLivro.autor = outroAutor;
-
-        outroLivro.mostrarDetalhes();
     }
 }
