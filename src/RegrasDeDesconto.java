685d259835895d6881e1edefdc59ab3c4b02afbb
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-07-25 14:29:45 -0300

    Incluindo variável desconto.

--- src/RegrasDeDesconto.java
+++ src/RegrasDeDesconto.java
@@ -1,11 +1,14 @@
 public class RegrasDeDesconto {
     public static void main(String[] args) {
+        
+        double desconto = 0.4;
+
         Livro livro = new Livro();
         livro.valor = 59.90;
 
         System.out.println("Valor atual: " + livro.valor);
 
-        if (!livro.aplicaDescontoDe(0.1)){
+        if (!livro.aplicaDescontoDe(desconto)){
             System.out.println("Desconto não pode ser maior do que 30%.");
         } else {
             System.out.println("Valor com desconto: " + livro.valor);
