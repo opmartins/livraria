df4dcb5326c50d7a49f1a4af81520ceaa496b50a
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-07-25 14:25:00 -0300

    Nova classe main para teste de descontos.

---
+++ src/RegrasDeDesconto.java
@@ -0,0 +1,14 @@
+public class RegrasDeDesconto {
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+        livro.valor = 59.90;
+
+        System.out.println("Valor atual: " + livro.valor);
+
+        if (!livro.aplicaDescontoDe(0.1)){
+            System.out.println("Desconto não pode ser maior do que 30%.");
+        } else {
+            System.out.println("Valor com desconto: " + livro.valor);
+        }
+    }
+}
