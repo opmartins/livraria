cf2fde7efe459c629dd72558a61b62c6476ed124
opmartins <opmartins@outlook.com>
2025-07-27 20:15:40 -0300

    Adicionando métodos da classe Ebook.

--- src/Ebook.java
+++ src/Ebook.java
@@ -1,5 +1,17 @@
 public class Ebook extends Livro{
+    
+    private String waterMark;
+    
     public Ebook (Autor autor){
         super (autor);
     }
+
+    public void setWaterMark(String waterMark) {
+        this.waterMark = waterMark;
+    }
+
+    public String getWaterMark(){
+        return waterMark;
+    }
+
 }
