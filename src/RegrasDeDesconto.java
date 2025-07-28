abec46301e71a2b44659ea0422f9ddc6cc057fe8
opmartins <opmartins@outlook.com>
2025-07-27 21:37:20 -0300

    Criando lógica da classe RegrasDeDesconto.

--- src/RegrasDeDesconto.java
+++ src/RegrasDeDesconto.java
@@ -1,19 +1,28 @@
 public class RegrasDeDesconto {
     public static void main(String[] args) {
         
-        double desconto = 0.4;
+        double descontoFisico = 0.3;
+        double descontoEbook = 0.15;
 
         Autor autor = new Autor();
         Livro livro = new Livro(autor);
         
         livro.setValor(59.90);
 
-        System.out.println("Valor atual: " + livro.getValor());
-
-        if (!livro.aplicaDescontoDe(desconto)){
-            System.out.println("Desconto não pode ser maior do que 30%.");
+        if (!livro.aplicaDescontoDe(descontoFisico)){
+            System.out.println("Desconto no livro não pode ser maior do que 30%.");
         } else {
-            System.out.println("Valor com desconto: " + livro.getValor());
+            System.out.println("Valor do livro com desconto: " + livro.getValor());
         }
+
+        Ebook ebook = new Ebook(autor);
+        ebook.setValor(29.90);
+
+        if (!ebook.aplicaDescontoDe(descontoEbook)){
+            System.out.println("Desconto no livro não pode ser maior do que 15%.");
+        } else {
+            System.out.println("Valor do ebook com desconto: " + ebook.getValor());
+        }
+
     }
 }
